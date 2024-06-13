ca609765db1c586a0b9c1f4bbb341f08ba3ac942
André Prado Junior <116444319+Andrepjr@users.noreply.github.com>
2024-06-13 10:16:42 -0300

    Alterando o NavBar para que ele fique fixo na tela

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -36,10 +36,10 @@ export default function Navbar() {
 
   if (usuario.token !== "") {
     return (
-      <Disclosure as="nav" className="bg-gray-800">
+      <Disclosure as="nav" className="bg-gray-800 fixed w-full z-10">
         {({ open }) => (
           <>
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-18">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-18 ">
               <div className="relative flex h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-6 text-white hover:bg-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
